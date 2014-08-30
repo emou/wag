35994ff5283b76aa795c1a3243a83ff16ea4b4ad
Emil Stanchev <stanchev.emil@gmail.com>
2014-08-30 18:59:01 +0300

    Add om dependency

--- project.clj
+++ project.clj
@@ -4,19 +4,24 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main wag.main
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.cli "0.2.2"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [log4j "1.2.17" :exclusions [javax.mail/mail
-                                              javax.jms/jms
-                                              com.sun.jdmk/jmxtools
-                                              com.sun.jmx/jmxri]]
-                 [compojure "1.1.5"]
-                 [ring-server "0.2.8"]
-                 [http-kit "2.1.5"]
-                 [clj-wamp "1.0.0-rc1"]
-                 [org.clojure/clojurescript "0.0-2197"
-                  :exclusions [org.apache.ant/ant]]]
+  :dependencies [
+    [org.clojure/clojure "1.6.0"]
+    [org.clojure/tools.cli "0.2.2"]
+    [org.clojure/tools.logging "0.2.6"]
+    [log4j "1.2.17" :exclusions [javax.mail/mail
+                                javax.jms/jms
+                                com.sun.jdmk/jmxtools
+                                com.sun.jmx/jmxri]]
+    [compojure "1.1.5"]
+    [ring-server "0.2.8"]
+    [http-kit "2.1.5"]
+    [clj-wamp "1.0.0-rc1"]
+
+    ; Client-side deps
+    [org.clojure/clojurescript "0.0-2197"
+    :exclusions [org.apache.ant/ant]]
+    [om "0.7.1"]
+  ]
   :plugins [[lein-cljsbuild "1.0.3"]
             [cider/cider-nrepl "0.7.0"]
             [org.clojure/tools.nrepl "0.2.4"]]
